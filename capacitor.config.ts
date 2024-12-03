c182fb1db6121e3bf9b5033f5d6876e451adecc5
Sebastián Cano <sclopez1807@gmail.com>
2024-12-03 18:42:51 -0500

    Navegación

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'habitosPlus',
+  appId: 'com.example.app',
+  appName: 'habitos_plus',
   webDir: 'www'
 };
 
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,26 +11,26 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
   },
-  {
-    path: 'create-habit',
-    loadChildren: () => import('./pages/create-habit/create-habit.module').then(m => m.CreateHabitPageModule)
-  },
-  {
-    path: 'progress',
-    loadChildren: () => import('./pages/progress/progress.module').then(m => m.ProgressPageModule)
-  },
-  {
-    path: 'reminders',
-    loadChildren: () => import('./pages/reminders/reminders.module').then(m => m.RemindersPageModule)
-  },
-  {
-    path: 'habits',
-    loadChildren: () => import('./pages/habits/habits.module').then(m => m.HabitsPageModule)
-  },
-  {
-    path: 'statics',
-    loadChildren: () => import('./pages/statics/statics.module').then(m => m.StaticsPageModule)
-  }
+   {
+     path: 'create-habit',
+     loadChildren: () => import('./pages/create-habit/create-habit.module').then(m => m.CreateHabitPageModule)
+   },
+   {
+     path: 'progress',
+     loadChildren: () => import('./pages/progress/progress.module').then(m => m.ProgressPageModule)
+   },
+   {
+     path: 'reminders',
+     loadChildren: () => import('./pages/reminders/reminders.module').then(m => m.RemindersPageModule)
+   },
+   {
+     path: 'habits',
+     loadChildren: () => import('./pages/habits/habits.module').then(m => m.HabitsPageModule)
+   },
+   {
+     path: 'statics',
+     loadChildren: () => import('./pages/statics/statics.module').then(m => m.StaticsPageModule)
+   }
 ];
 
 @NgModule({
